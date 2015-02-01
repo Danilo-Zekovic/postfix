f08c67c85e4d1a4044ed8647197a585cbf116b9a
Danilo Zekovic <dzv9776@saintjoe.edu>
2015-02-01 18:32:20 -0500

    Debuging the infix to postfix left

--- postfix.js
+++ postfix.js
@@ -9,12 +9,13 @@
 // stack constructor
 function Stack() {
   this.dataStore = [];
-  this.top = [];
+  this.top = 0;
   this.push = push;
   this.pop = pop;
   this.peek = peek;
   this.clear = clear;
-  this.getToken = getToken;
+  this.length = length;
+  //this.getToken = getToken;
 }
 
 function push(element) {
@@ -40,15 +41,86 @@ function clear() {
 
 function getToken() {
   print("getToken");
+  var postStr = "";
+  var operators = new Stack();
   for (var i = 0; i<input.length; ++i){
     var x = input.charAt(i); 
-    print(x);
-    // if
+    print("x: " + x);
+    // if it is not a number
+    if(isNaN(x)){
+      
+      if (operators.length() == 0 || operators.peek() == '('){  //2
+	print(2);
+        operators.push(x);
+      }else if (x == '('){   // 3
+        print(3);
+        operators.push(x); 
+      }else if (x == ')') {       //4
+	print(4);
+	var y = operators.pop();
+        while( y != '('){
+	  postStr = postStr + y;
+	  y = operators.pop();
+	}
+      }else if (sVal(operators.peek()) < sVal(x) ){     // 5
+	print(5);
+        operators.push(x);
+      }else if(sVal(operators.peek()) == sVal(x)){	      //6
+	print(6 + operators.peek());
+        postStr == postStr + operators.pop();
+	print(6.1 + operators.peek() + postStr);
+	operators.push(x);
+	print(6.2 + operators.peek());
+      }else if(sVal(operators.peek()) > sVal(x)){        // 7
+	print(7);
+        while(sVal(operators.peek()) > sVal(x) && operators.length != 0){
+	  print(7.1);
+	  postStr = postStr + operators.pop();
+	}
+	operators.push(x);
+      }
+
+
+
+/*
+      // something
+      //print("not # " + x);
+      if(x == '*' || x == '/'){
+        operators.push(x);
+      }else{
+        if(operators.peek()=='*' || operators.peek()=='/'){
+	  //sdfsd
+	  postStr = postStr + operators.pop();
+	  operators.push(x); 
+	}else{
+	  operators.push(x);
+	}       
+      }*/
+    }else{         // if it is
+      //sadasd
+      //print("is number " + x);
+      postStr = postStr + " " + x;
+    }
   }
+  
+  // empty the stack and add all of the operators to the string
+  print(operators.length());
+  while (operators.length() >  0){
+    postStr = postStr + operators.pop();
+  }
+  return postStr;
 }
 
 
 
+
+function sVal(symbol){
+  if(symbol == '+' || symbol == '-'){
+    return 0;
+  }else if (symbol == '*' || symbol == '/'){
+    return 1;
+  }
+}
 
 
 
@@ -56,15 +128,11 @@ function getToken() {
 // prompt the user for expresion
 var input = readline();
 print(input);
-var foo = new Stack();
+//var foo = new Stack();
 
 
-foo.getToken(input);
-
-
-var j = 2;
-var s = 3;
-var so = j.concat(s);
-
+//foo.getToken(input);
+var foo = getToken(input);
+print(foo);
 print("the end");
 
