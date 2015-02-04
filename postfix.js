8ab84a2b873278270c48e408cd661c8380f5ad31
Danilo Zekovic <dzv9776@saintjoe.edu>
2015-02-04 01:16:43 -0500

    deleted debugging, added some coments;

--- postfix.js
+++ postfix.js
@@ -15,7 +15,6 @@ function Stack() {
   this.peek = peek;
   this.clear = clear;
   this.length = length;
-  //this.getToken = getToken;
 }
 
 function push(element) {
@@ -40,15 +39,18 @@ function clear() {
 //-------------------------------------------
 // function to turn infix to postfix
 function getToken() {
-  //print("getToken");
+
+  // for menaging multy digit numbers
   var number = "";
   var postStr = "";   // create an empty string to store final values
   // stack to push operators and pop them when needed
   var operators = new Stack(); 
+
   // loop through all the characters in inputed string
   for (var i = 0; i<input.length; ++i){
     var x = input.charAt(i); // get character by character
-    //print("x: " + x);
+
+    // separate numbers from other characters
     if( x == " "){
       // do nothing 
       continue;
@@ -58,17 +60,17 @@ function getToken() {
       postStr = character(operators,x,postStr);
       number = "";
     }else{         // if it is
-      //print("is number " + x);
       number = number + x;
-    }
-    //print(postStr);
-  }
+    }    // end if-else
+  }    // end for 
+
   if(number!=""){postStr = postStr + " " + number;}
+
   // empty the stack and add all of the operators to the string
-  //print(operators.length());
   while (operators.length() >  0){
     postStr = postStr + " " + operators.pop();
   }
+
   return postStr;
 }
 
@@ -88,21 +90,20 @@ function sVal(symbol){
 var character = function(operators, x, postStr){
   // if symbol is not pushed or added to string it stays true 
   var notSorted = true; 
+
   // loop until character is sorted
   while (notSorted){
+
       // if stack empty or "(" is at the top push operator to stack
       if (operators.length() == 0 || operators.peek() == '('){  //2
-	//print(2 + "ch");
-        operators.push(x);
+	operators.push(x);
 	notSorted = false;
       // if "(" push it to stack
       }else if (x == '('){   // 3
-        //print(3 + "ch");
         operators.push(x);
         notSorted = false;	
       // if ")" pop the stack til "("
       }else if (x == ')') {       //4
-	//print(4 + "ch");
 	var y = operators.pop();
         while( y != '('){
 	  postStr = postStr + " " + y;
@@ -112,51 +113,50 @@ var character = function(operators, x, postStr){
       // if incoming symbol has higher precedence then the top of the stack
       // push it to the stack
       }else if (sVal(operators.peek()) < sVal(x) ){     // 5
-	//print(5 + "ch");
-        operators.push(x);
+	operators.push(x);
 	notSorted = false;
       // if incoming symbo has same precedence as the top of the stack
       // pop it and then push the new one
       }else if(sVal(operators.peek()) == sVal(x)){	      //6
-	//print(6 + operators.peek());
-        postStr = postStr + " " + operators.pop();
-	//print(6.1 + operators.peek() + postStr);
+	postStr = postStr + " " + operators.pop();
 	operators.push(x);
-	//print(6.2 + operators.peek());
 	notSorted = false;
       // if the incoming symbol has lower precedence then the top of stack
       // pop the stack til it is not higher anymore and
       // repeat other steps
       }else if(sVal(operators.peek()) > sVal(x)){        // 7
-	//print(7 + "ch");
-        //print(7.1);
 	postStr = postStr + " " + operators.pop();
       } // end else if
   }// end while
       return postStr;
 } // end function
 
-// to evaluate the function
+// to evaluate the expresion
 var evaluate = function(string){
+
+  // array to elements split at space
   var stuff = string.trim().split(" ");
-  //print(stuff);
+  // stack to push the numbers that will be evaluated
   var evalStack = new Stack();
+  // to store the curent element from stuff
   var element = "";
+
   for (var i = 0; i < stuff.length; i++){
+    
     element = stuff[i];
-    //print(element + " this is before if");
+
+    // test is it a number or not
     if (!isNaN(element)){
       // if it is a number push it to the stack
       evalStack.push(element);
     }else if(element=="*" || element=="+" || element=="-" || element=="/"){
-      //
+      
+      // if it is a operator find the case and evaluate
       var part = 0; // one part of the expression that is evaluated
       switch(element){
         case ("*"):
 	  part = evalStack.pop() * evalStack.pop();
-	 // print(part + " part");
-	  evalStack.push(part);
-	  //print(evalStack.peek() + " peek");
+	  evalStack.push(part); 
 	  break;
 	case ("/"):
 	  var denominator = evalStack.pop();
@@ -172,24 +172,21 @@ var evaluate = function(string){
 	  part = parseInt(evalStack.pop()) - subtrahend;
 	  evalStack.push(part);
 	  break;
-      }
-    }
-  }
-  //print(evalStack.pop() + " final answear");
+      }    // end switch
+    }    // end if-else
+  }    // end for
   return evalStack.pop();
-}
+}    // end evaluate
 
+//------------------------------------------------------------------------
 
-
-
-//----------------------------------------------
 // prompt the user for expresion
 var input = readline();
 print("This is user input: " + input);
 
-var foo = getToken(input);
-print("Postfix:" + foo);
+var expression = getToken(input);
 
-print("Expresion evaluated: " + evaluate(foo));
-print("the end");
+print("Postfix:" + expression);
+print("Expresion evaluated: " + evaluate(expression));
+print(">>>>>>>>>> the end <<<<<<<<<<");
 
